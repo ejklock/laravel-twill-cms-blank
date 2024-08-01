de0e89a0bb597b1bb4709424a796b156ced1a2c9
Evaldo Klock <neto.nemesis@gmail.com>
2024-08-01 17:23:15 -0400

    feat: changing blog theme

--- resources/frontend/js/bootstrap.js
+++ resources/frontend/js/bootstrap.js
@@ -3,11 +3,12 @@
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
-
-import axios from 'axios';
+import jQuery from "jquery";
+window.$ = jQuery;
+import axios from "axios";
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
